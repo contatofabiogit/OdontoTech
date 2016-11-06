dfbedbd307fffd11340afe09fc212edbe717fef9
Fábio Alves Ribeiro <contato.fabioalves@outlook.com>
2016-11-06 21:49:58 -0200

    Dia 06-11: Commit 02 - Começando a produção dos relatorios de pacientes

--- dentista/relatorio-paciente.php
+++ dentista/relatorio-paciente.php
@@ -1,8 +1,35 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include ('../pdf/mpdf.php');
 
+//Executando consulta sql
+$result1 = mysql_query("SELECT * FROM pessoa WHERE nivel_acesso = 'Paciente'");
+
+$resultado1 = mysql_fetch_assoc($result1);
+
+$pagina = "
+    <html>
+        <body>
+          <h1> Relatório de Pacientes</h1>
+          
+          Cód. = ".$resultado1['id_pessoa']." <br />
+          Nome = ".$resultado1['nome']."<br />
+          Login = ".$resultado1['login']."<br />
+          E-mail = ".$resultado1['email']."<br />
+          Data de Nascimento = ".$resultado1['dt_nascimento']."<br />
+          Telefone = ".$resultado1['telefone']."<br />
+          
+        </body>
+        <h4>http://www.mtechsolucoes.com.br</h4>
+    </html>
+  ";
+
+  $arquivo = "cadastro01.pdf";
+
+  $mpdf = new mPDF();
+
+  $mpdf->WriteHTML($pagina);
+
+  $mpdf->Output($arquivo, 'I');
+
+?>
